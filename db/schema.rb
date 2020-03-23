61aed989f72d64ab3ee962ccf9cc3b6f4962feda
Ethan Davidson <theejdavidson@gmail.com>
2020-03-23 15:52:49 -0500

    seed file

---
+++ db/schema.rb
@@ -0,0 +1,55 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_03_23_203857) do
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "attack"
+    t.integer "defense"
+    t.string "img_url"
+    t.string "card_type"
+    t.integer "level"
+    t.text "description"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "race"
+    t.string "attribute"
+  end
+
+  create_table "deck_cards", force: :cascade do |t|
+    t.integer "card_id", null: false
+    t.integer "deck_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["card_id"], name: "index_deck_cards_on_card_id"
+    t.index ["deck_id"], name: "index_deck_cards_on_deck_id"
+  end
+
+  create_table "decks", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_decks_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "deck_cards", "cards"
+  add_foreign_key "deck_cards", "decks"
+  add_foreign_key "decks", "users"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'net/http'
+require 'open-uri'
+require 'json'
+require_relative '../config/environment.rb'
+
+models = [Card]
+
+models.each { |model| model.destroy_all }
+
+API_BASE= 'https://db.ygoprodeck.com/api/v6/'
+
+def validate_api_response(response)
+    case response
+    when Net::HTTPSuccess then
+        response
+    else
+        raise response.value
+    end
+end
+
+def get_all_card_data
+    uri = URI.parse(API_BASE + 'cardinfo.php')
+    response = Net::HTTP.get_response(uri)
+
+    response = validate_api_response(response)
+
+    JSON.parse(response.body)
+end
+
+def single_card_test
+    uri = URI.parse('https://db.ygoprodeck.com/api/v6/cardinfo.php?name=Decode%20Talker')
+    response = Net::HTTP.get_response(uri)
+
+    response = validate_api_response(response)
+
+    JSON.parse(response.body)
+end
+
+def model_cards
+    data = get_all_card_data
+
+    data.each do |card|
+        create_card(card)
+    end
+end
+
+def create_card(data)
+    name = data["name"]
+    attack = data["atk"]
+    defense = data["def"]
+    #card_type = data["type"]
+
+    Card.create(name: name, attack: attack, defense: defense)
+end
+
+# t.integer "attack"
+#     t.integer "defense"
+#     t.string "img_url"
+#     t.string "card_type"
+#     t.integer "level"
+#     t.text "description"
+#     t.string "name"
+    # t.string "race"
+    # t.string "attribute"
+
+model_cards
