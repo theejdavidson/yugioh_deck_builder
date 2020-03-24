4b06a109b5a81e7889204fce8a73068a93256579
deber116 <deber116@gmail.com>
2020-03-24 17:10:20 -0500

    basic login/out functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        if session[:user_id]
+          @user = User.find_by(id: session[:user_id])
+        else
+        end
+    end
+      
+      def logged_in?
+        !!current_user
+      end
+    
+      def authorized
+        redirect_to login_path unless logged_in?
+      end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+    before_action :authorized, only: [:index]
+    
+    def index
+        @current_user = current_user
+        @users = User.all
+    end
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to users_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        
+        @user = current_user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+end
